ccd3a9b6832b89a4942507ef263d5cf91f51fe19
Griffin Bishop <grbishop@wpi.edu>
2017-01-25 11:00:41 -0500

    Read_board function somewhat fixed

--- assign1.py
+++ assign1.py
@@ -11,11 +11,14 @@ def gen_board(n, m):
 
 
 def print_board(board):
+    bstr= ""
     for row in board:
         rowStr = ""
         for spot in row:
             rowStr+= spot + "\t"
-        print(rowStr)
+        bstr += rowStr
+    print(bstr)
+        #print(rowStr)
 
 # Save board to file
 
@@ -28,27 +31,46 @@ def save_board(board, filename):
         f.write(rowStr + '\n')
     f.close()
 
+##def read_board(filename):
+##    f = open(filename, 'r')
+##    boardStr = f.read(1000)
+##    chars = boardStr.split('\t')
+##
+##    i = 0
+##    newboard = []
+##    j = 0
+##    while i < len(chars):
+##        char = '#'
+##        tmp = []
+##        
+##        while char != '\n':
+##            char = chars[j]
+##            i += 1
+##            j += 1
+##            #print(char)
+##            tmp += char
+##        newboard += [tmp]
+##        
+##    return newboard
+
 def read_board(filename):
     f = open(filename, 'r')
     boardStr = f.read(1000)
     chars = boardStr.split('\t')
 
-    i = 0
-    newboard = []
-    j = 0
-    while i < len(chars):
-        char = '#'
-        tmp = []
-        
-        while char != '\n':
-            char = chars[j]
-            i += 1
-            j += 1
-            #print(char)
-            tmp += char
-        newboard += [tmp]
-        
-    return newboard
+    board = []
+    row = []
+    for c in chars:
+        if "\n" in c:
+            board += [row]
+            row = []
+        else:
+            row += c
+
+    f.close()
+    return board
+            
+
 
 
     
